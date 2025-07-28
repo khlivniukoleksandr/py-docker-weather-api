e260ec6d7072958b6ac3e7b639e8cc67c7c20037
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-07-28 20:36:06 +0300

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 import requests
 
@@ -11,21 +13,25 @@ BASE_URL = "http://api.weatherapi.com/v1/current.json"
 
 
 def get_weather() -> None:
-    params = {
-        "key": API_KEY,
-        "q": CITY,
-        "lang": "en"
-    }
-    response = requests.get(BASE_URL, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        location = data["location"]["name"]
-        temperature = data["current"]["temp_c"]
-        condition = data["current"]["condition"]["text"]
-        print(f"Weather in {location}: {temperature}°C, {condition}")
+    if not API_KEY:
+        print("Error, not API KEY")
+        sys.exit(1)
     else:
-        print("An error occurred, unable to retrieve data")
+        params = {
+            "key": API_KEY,
+            "q": CITY,
+            "lang": "en"
+        }
+        response = requests.get(BASE_URL, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            location = data["location"]["name"]
+            temperature = data["current"]["temp_c"]
+            condition = data["current"]["condition"]["text"]
+            print(f"Weather in {location}: {temperature}°C, {condition}")
+        else:
+            print("An error occurred, unable to retrieve data")
 
 
 if __name__ == "__main__":
